01013c3c033756069f64d84b681ef4c3c2f8c60e
q_y_l <956441594@qq.com>
2020-05-12 19:39:19 +0800

    一个使用按钮启动其他项目的例子

---
+++ RunOthers/HelloBoy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloBoy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World! Now is {DateTime.Now}");
+            Thread.Sleep(4000);
+            Console.WriteLine($"Time is :{DateTime.Now}");
+         //   Console.ReadKey();
+        }
+    }
+}
---
+++ RunOthers/RunOthers/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace RunOthers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public readonly Process hello=new Process();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startHello(object sender, RoutedEventArgs e)
+        {
+            hello.StartInfo.FileName = @"C:\Users\QYL\source\repos\RunOthers\HelloBoy\bin\Debug\netcoreapp3.1\HelloBoy.exe";
+            hello.StartInfo.Arguments = "";
+            hello.Start();
+            Stop.IsEnabled = true;
+            Start.IsEnabled = false;
+        }
+
+        private void stopHello(object sender, RoutedEventArgs e)
+        {
+            hello.CloseMainWindow();
+            //  hello.Close();
+            hello.Kill();
+            Stop.IsEnabled = false;                                                                                                                                
+            Start.IsEnabled = true;
+        }
+    }
+}
